b46a7cbc70e42e9a58bf3519b5751ae4f45c3adb
vuode <byzov.iv@gmail.com>
2020-04-28 03:41:25 +0300

    Fix cors fix

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 export default function corsMiddleware() {
   return function (req: Request, res: Response, next: NextFunction) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next()
   }
 }
