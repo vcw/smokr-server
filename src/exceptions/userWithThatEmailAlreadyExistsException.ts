d6e74444254e2838ef168af4927b1e857ebed5dd
vcw <vcw@users.noreply.github.com>
2020-10-23 11:52:34 +0300

    Remove unused exceptions

--- src/exceptions/userWithThatEmailAlreadyExistsException.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from "./httpException";
-
-class UserWithThatEmailAlreadyExistsException extends HttpException {
-  constructor(email: string) {
-    super(400, `User with email ${email} already exists`);
-  }
-}
-
-export default UserWithThatEmailAlreadyExistsException;
--- src/exceptions/wrongCredentialsException.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from "./httpException";
-
-class WrongCredentialsException extends HttpException {
-  constructor() {
-    super(401, 'Wrong credentials provided');
-  }
-}
-
-export default WrongCredentialsException;
